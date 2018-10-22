50590f72c2361b8d70e8292290516bffebd610c8
longhe.li <lilonghe@outlook.com>
2018-10-22 21:00:41 +0800

    add redux debug tool

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import { apps } from "./reducer/apps";
 import { appDetail } from './reducer/appDetail';
 import thunk from 'redux-thunk';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 let store = createStore(
     combineReducers({ apps, appDetail }),
+    /* preloadedState, */ composeEnhancers(
     applyMiddleware(thunk)
-);
+));
 
 export default store;
